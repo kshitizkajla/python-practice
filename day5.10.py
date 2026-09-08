afa21795be660a22401c73ced31a92beca58d648
kshitiz-star <2006kshitiz@gmail.com>
2026-09-08 20:48:31 +0530

    day 5: lecture 2 basics (dictionary)

---
+++ day5.10.py
@@ -0,0 +1,10 @@
+students = {
+    "Hermoine": "Gryffindor",
+    "Harry": "Gryffindor",
+    "Ron": "Gryffindor",
+    "Draco": "Slytherin"
+}
+print(students["Hermoine"])
+print(students["Harry"])
+print(students["Ron"])
+print(students["Draco"])
